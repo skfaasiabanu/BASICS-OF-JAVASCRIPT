8209508ebf63a7efc01d8697b93b8f70fa25139e
SHAIK FAASIA BANU <skfaasiabanu178@gmail.com>
2024-09-17 18:42:59 +0530

    yh

---
+++ 06readlinesync.js
@@ -0,0 +1,5 @@
+//read data from user
+const input=require("reading-sync");//initiallizing a modulee readline-sync
+const userName= input.question("may i know your name");//question is a property of readline-sync package
+//console.log(userName);
+    
