474e5ca4bf1dc3d18f2a191f0b22fa586d8c745d
SHAIK FAASIA BANU <skfaasiabanu178@gmail.com>
2024-09-17 18:41:37 +0530

    OKK

---
+++ 03arithmeticoperators.js
@@ -0,0 +1,43 @@
+
+       /* const x=10;
+        const y=5;
+        console.log(x+y);//addition operator
+        console.log(x-y);
+        console.log(x*y);
+        console.log(x/y);
+        console.log(x%y);
+        console.log(x**2);//power of 2
+        console.log(y**3);//cubic of a number*/
+
+        /*const x="10";
+        const y="5";
+        console.log(x+y);//addition operator
+        console.log(x-y);
+        console.log(x*y);
+        console.log(x/y);
+        console.log(x%y);
+        console.log(x**2);//power of 2
+        console.log(y**3);//cubic of a number*/
+
+        /*const x=10;
+        const y="5";
+        console.log(x+y);//addition operator
+        console.log(x-y);
+        console.log(x*y);
+        console.log(x/y);
+        console.log(x%y);
+        console.log(x**2);//power of 2
+        console.log(y**3);//cubic of a number*/
+
+
+        const x="faasia";
+        const y="banu";
+        console.log(x+y);//addition operator
+        console.log(x-y);
+        console.log(x*y);
+        console.log(x/y);
+        console.log(x%y);
+        console.log(x**2);//power of 2
+        console.log(y**3);//cubic of a number
+
+   
