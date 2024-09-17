83c0f9c89ea67fc33cd052418b9b8b8cee1aa8ba
SHAIK FAASIA BANU <skfaasiabanu178@gmail.com>
2024-09-17 18:40:55 +0530

    AS

---
+++ 01concatenationandtemplateliterals.js
@@ -0,0 +1,47 @@
+        console.log("hello world");
+        console.log("my name is faasia");
+        console.log(7+3);
+        var message;
+        message="hello greeks";
+        message="hello faasia";
+        console.log(message);
+
+        let text;
+        text="javascript is the best";
+        console.log(text);
+
+
+        let myname="my name is faasia";
+        console.log(myname);
+
+        const color="blue";
+        console.log("the sky is"+color+"in the  morning");
+        
+
+        let userName="faasia";
+        console.log(userName);
+        console.log(typeof userName);
+        console.log(typeof "faasia");
+
+        let age=99.5;
+        console.log(typeof age);
+
+
+        let isProductInCart=false;
+        console.log(typeof isProductInCart);
+
+
+        let lastNmae;
+        console.log(typeof lastName);
+
+        let  noValue=null;
+        console.log(typeof noValue);
+        console.log(noValue);
+
+        const personDetails={name:"faasia", age:99, education:"engineer"}
+        console.log(personDetails);
+        console.log(typeof personDetails);
+
+        const arrayOfNumbers=[1,2,3,4,5];
+        console.log(arrayOfNumbers);
+        console.log(typeof arrayOfNumbers);
