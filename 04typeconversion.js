fdba9f8679210c41e5464184842a7dd1747a440a
SHAIK FAASIA BANU <skfaasiabanu178@gmail.com>
2024-09-17 18:42:12 +0530

    gh

---
+++ 04typeconversion.js
@@ -0,0 +1,44 @@
+
+        const x="12";
+        const y="4";
+        console.log(x+y);
+        console.log(Number(x)+Number(y));
+        console.log(Number("12")+Number(y));
+
+        const c= Number(x);
+        const d=Number(y);
+        console.log(typeof x);
+        console.log(typeof y);
+        console.log(typeof c);
+        console.log(typeof d);
+
+
+        const e=10;
+        const f=2;
+        const g=String(e);
+        const h=String(f);
+        console.log(typeof e);
+        console.log(typeof f);
+        console.log(typeof g);
+        console.log(typeof h );
+        console.log(g);
+        console.log(h);
+
+        const j=10;
+        console.log(Boolean(j));
+
+        const s=0;
+        console.log(Boolean(s));
+
+        const q="faasia";
+        console.log(Boolean(q));
+
+        const l="";
+        console.log(Boolean(l));
+
+
+
+
+
+
+    
