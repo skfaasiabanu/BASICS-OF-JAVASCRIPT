db3795b2b9a0bce379c9d87ae314d801496e8ce0
SHAIK FAASIA BANU <skfaasiabanu178@gmail.com>
2024-09-17 18:41:21 +0530

    SA

---
+++ 02basics.js
@@ -0,0 +1,20 @@
+
+        //conactenation
+        let userName="faasia";
+        let age=19;
+        let message="My name is "+userName+"and ia am "+age+" years old";
+        console.log(message);
+
+
+
+        //template literals(it is used for readability)
+        let text=`My name is ${userName} and iam ${age} years old`;
+        console.log(text);
+
+        //template
+        let multiple=`my name is ${userName}
+iam ${age} years old
+i love coding
+i love to eat
+i love to sing`
+        console.log(multiple);
