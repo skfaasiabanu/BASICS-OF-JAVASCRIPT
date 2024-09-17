07f6b0ee0dec1120f7dedafb8ddc08bddc880cc3
SHAIK FAASIA BANU <skfaasiabanu178@gmail.com>
2024-09-17 18:42:32 +0530

    we

---
+++ 05practiceartithmetic.js
@@ -0,0 +1,9 @@
+
+    const userInput = "42"; // Simulating user input
+const numberInput = Number(userInput);
+
+if (!isNaN(numberInput)) {
+    console.log(numberInput + 8); // Outputs: 50
+} else {
+    console.log('Invalid input');
+}
